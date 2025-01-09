df481ce6f78a3f4c5bea05273ae42895f33d8c0d
Prathmesh Urkude <Prathmeshurkude20@gmail.com>
2025-01-10 00:27:29 +0530

    created the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,11 +25,11 @@ export default function Header() {
       </Button>
 
       <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill > 
+        <Button className='w-12 h-10 hidden sm:inline ' color='gray' pill > 
           <FaMoon />
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='greenToBlue' pill >
+          <Button gradientDuoTone='greenToBlue' outline>
             Sign In
           </Button>
         </Link>
